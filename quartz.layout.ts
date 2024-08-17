2f736262d995aac2a03ff6e9d89f1741022a3d1d
xyang <121338860+xyang8@users.noreply.github.com>
2024-08-17 13:32:31 +0200

    Fix footer build

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,7 +6,9 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: [],
+  footer: Component.Footer({
+    links:
+  }),
 }
 
 // components for pages that display a single page (e.g. a single note)
